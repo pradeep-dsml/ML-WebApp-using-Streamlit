ea81a2cf72b937215690fc6cd8db2268673aed49
PRADEEP YADAV <74373064+pradeep-dsml@users.noreply.github.com>
2021-05-27 19:46:23 +0530

    Create sample_app.py

---
+++ apps/sample_app.py
@@ -0,0 +1,13 @@
+# all imports goes here
+
+
+def app():
+  # all
+  #code
+  #goes
+  # here
+
+  """
+  don't forget to add app name in main app.py file
+  and requred libraby in requrements.txt
+  """
